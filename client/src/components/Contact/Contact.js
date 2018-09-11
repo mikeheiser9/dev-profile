0f831b89025ec3b47fbd6f345751cc5f40c5bc03
mikeheiser9 <mikeheiser9@gmail.com>
2018-09-11 15:33:12 -0700

    work and contact

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Particles from 'react-particles-js';
-import {Dropdown, Button, NavItem, Modal, Col, Row, Navbar} from "react-materialize"
+import {Dropdown, Button, NavItem, Modal, Col, Row, Input, Navbar} from "react-materialize"
 import "../Contact/Contact.css";
 
 
@@ -9,7 +9,13 @@ class Contact extends Component{
     render(){
         return (
             <div className={"contactContainer"} id={"contact"}>
-            
+            <Row>
+                <Input s={6} label="First Name" />
+                <Input s={6} label="Last Name" />
+                <Input s={6} label="Phone Number"  />
+                <Input placeholder="Email" label="Email" s={6} />
+                <Input type="textarea" label="Tell me what you are looking for" s={12} />
+            </Row>
             
             </div>
         );
--- client/src/components/Work/Work.js
+++ client/src/components/Work/Work.js
@@ -10,7 +10,23 @@ class Work extends Component{
         return (
             <div className={"workContainer"} id={"work"}>
             <Row>
-                
+                <p className="work-head">Some of my Recent Work</p>
+            </Row>
+            <Row className={"gif-container"}>
+                <Col s={12} m={6} l={6} xl={6} className={"gif-img-cont"}>
+                <img src={'../../../images/baddlabs.gif'} alt={"baddlabs-gif"} className={"gif-img"} />
+                </Col>
+                <Col s={12} m={6} l={6} xl={6} className={"gif-img-cont"}>
+                <img src={'../../../images/hrf.gif'} alt={"hrf-gif"} className={"gif-img"} />
+                </Col>  
+            </Row>
+            <Row className={"gif-container"}>
+                <Col s={12} m={6} l={6} xl={6} className={"gif-img-cont"}>
+                <img src={'../../../images/dive.gif'} alt={"dive-gif"}className={"gif-img"} />
+                </Col>
+                <Col s={12} m={6} l={6} xl={6} className={"gif-img-cont"}>
+                <img src={'../../../images/squadup.gif'} alt={"squad-gif"} className={"gif-img"} />
+                </Col>  
             </Row>
             </div>
 
